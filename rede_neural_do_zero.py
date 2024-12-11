0c9599832ddb02b397deee0f05524fcce4eb80e5
Alex <alexander97643@gmail.com>
2024-12-11 20:52:14 -0300

    update

--- rede_neural_do_zero.py
+++ rede_neural_do_zero.py
@@ -14,3 +14,8 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 valset = datasets.MNIST('./MNIST_data/', download=True, train=False, transform=transform) # Carrega a parte da validação 
 valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)# Cria um buffer para pegar os dados
+
+dataiter = iter(trainloader)
+imagens, etiquetas = dataiter._next_data()
+plt.imshow(imagens[0].numpy().squeeze(), cmap='gray_r')
+plt.show()
