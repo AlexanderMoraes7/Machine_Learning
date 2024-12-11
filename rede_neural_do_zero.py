4508b8858a252def8a1cbce168a2bd216cb6e770
Alex <127699880+AlexanderMoraes7@users.noreply.github.com>
2024-12-11 20:36:45 -0300

    Add files via upload

---
+++ rede_neural_do_zero.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+transform = transforms.ToTensor() # Definindo a conversão de imagem para tensor
+
+trainset = datasets.MNIST('./MNIST_data/', download=True, train=True, transform=transform) # Carrega parte de treino do dataset
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True) # Cria um buffer para pegar os dados por partes
+
+valset = datasets.MNIST('./MNIST_data/', download=True, train=False, transform=transform) # Carrega a parte da validação 
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)# Cria um buffer para pegar os dados
