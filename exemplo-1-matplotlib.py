e7b501a38bc74cce4245895a3211efd30e2e5940
Alex <alexander97643@gmail.com>
2024-12-19 21:17:54 -0300

    update

--- exemplo-1-matplotlib.py
+++
@@ -1,7 +0,0 @@
-from sklearn.datasets import make_regression
-import matplotlib
-import matplotlib.pyplot as plt
-
-x, y = make_regression(n_samples=200,n_features=1, noise=30)
-plt.scatter(x,y)
-plt.show()
